703fc3e5ad40c4bb5a664afd8574a282f6163545
lilygrierjil <87042447+lilygrierjil@users.noreply.github.com>
2023-08-02 22:08:05 +0000

    EDIT used precommit

--- common-code/azure_wrappers/tests/test_az_wrappers.py
+++ common-code/azure_wrappers/tests/test_az_wrappers.py
@@ -13,19 +13,20 @@ from azure.core.exceptions import ResourceNotFoundError
 from ..azure_container import *
 
 DEFAULT_ACCOUNT_URL = "YOUR_ACCOUNT_URL"
-TEST_ACCOUNT_URL = os.environ.get('ACCOUNT_URL', DEFAULT_ACCOUNT_URL)
+TEST_ACCOUNT_URL = os.environ.get("ACCOUNT_URL", DEFAULT_ACCOUNT_URL)
 DEFAULT_TEST_CONTAINER = "YOUR_TEST_CONTAINER"
-TEST_CONTAINER = os.environ.get('CONTAINER_NAME', DEFAULT_TEST_CONTAINER)
+TEST_CONTAINER = os.environ.get("CONTAINER_NAME", DEFAULT_TEST_CONTAINER)
+
 
 def test_upload_blob_with_metadata():
     upload_to_az(
         pd.DataFrame(
-    {
-        "A": ["foo", "bar", "foo", "bar", "foo", "bar", "foo", "foo"],
-        "B": ["one", "one", "two", "three", "two", "two", "one", "three"],
-        "C": np.random.randn(8),
-        "D": np.random.randn(8),
-    }
+            {
+                "A": ["foo", "bar", "foo", "bar", "foo", "bar", "foo", "foo"],
+                "B": ["one", "one", "two", "three", "two", "two", "one", "three"],
+                "C": np.random.randn(8),
+                "D": np.random.randn(8),
+            }
         ),
         TEST_ACCOUNT_URL,
         TEST_CONTAINER,
@@ -35,31 +36,34 @@ def test_upload_blob_with_metadata():
         auto_overwrite=True,
     )
 
+
 def test_get_data():
     data = get_az_data(TEST_ACCOUNT_URL, TEST_CONTAINER, "test_metadata_upload.txt")
 
+
 def test_upload_files_from_folder():
     upload_files_from_folder(
-        TEST_ACCOUNT_URL, TEST_CONTAINER, 'data/', dest_folder_name="test_uploads"
+        TEST_ACCOUNT_URL, TEST_CONTAINER, "data/", dest_folder_name="test_uploads"
     )
 
+
 @pytest.mark.parametrize(
-"account_url, container_name, file_name",
-[
-    pytest.param(
-        TEST_ACCOUNT_URL,
-        TEST_CONTAINER,
-        "test_other_types_folder/auto.dta",
-        marks=pytest.mark.xfail(reason="utf-8 codec cannot decode auto.dta"),
-    ),
-    (TEST_ACCOUNT_URL, TEST_CONTAINER, "test_uploads/dummy_data.csv"),
-    (TEST_ACCOUNT_URL, TEST_CONTAINER, "test_uploads/sample_image.jpg"),
-    (TEST_ACCOUNT_URL, TEST_CONTAINER, "test_uploads/sample_img.png"),
-    (TEST_ACCOUNT_URL, TEST_CONTAINER, "test_uploads/sample.pdf"),
-    (TEST_ACCOUNT_URL, TEST_CONTAINER, "test_uploads/test_text.txt"),
-    (TEST_ACCOUNT_URL, TEST_CONTAINER, "test_uploads/test.tsv"),
-    (TEST_ACCOUNT_URL, TEST_CONTAINER, "test_parq.parquet"),
-],
+    "account_url, container_name, file_name",
+    [
+        pytest.param(
+            TEST_ACCOUNT_URL,
+            TEST_CONTAINER,
+            "test_other_types_folder/auto.dta",
+            marks=pytest.mark.xfail(reason="utf-8 codec cannot decode auto.dta"),
+        ),
+        (TEST_ACCOUNT_URL, TEST_CONTAINER, "test_uploads/dummy_data.csv"),
+        (TEST_ACCOUNT_URL, TEST_CONTAINER, "test_uploads/sample_image.jpg"),
+        (TEST_ACCOUNT_URL, TEST_CONTAINER, "test_uploads/sample_img.png"),
+        (TEST_ACCOUNT_URL, TEST_CONTAINER, "test_uploads/sample.pdf"),
+        (TEST_ACCOUNT_URL, TEST_CONTAINER, "test_uploads/test_text.txt"),
+        (TEST_ACCOUNT_URL, TEST_CONTAINER, "test_uploads/test.tsv"),
+        (TEST_ACCOUNT_URL, TEST_CONTAINER, "test_parq.parquet"),
+    ],
 )
 def test_download_data(account_url, container_name, file_name):
     try:
@@ -73,11 +77,11 @@ def test_download_data(account_url, container_name, file_name):
     except ResourceNotFoundError:
         pytest.mark.xfail(reason=f"{file_name} not found")
 
+
 def test_download_data_resource_error():
     with pytest.raises(ResourceNotFoundError):
-        data = get_az_data(
-            TEST_ACCOUNT_URL, "fakecontainer", "test_parq.parquet"
-        )
+        data = get_az_data(TEST_ACCOUNT_URL, "fakecontainer", "test_parq.parquet")
+
 
 def test_list_container_files():
     list_container_files(TEST_ACCOUNT_URL, TEST_CONTAINER)
